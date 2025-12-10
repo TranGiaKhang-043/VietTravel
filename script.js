073322ba40ad977617290dc9a6ba3fab8b85fb13
KhangTG <5101104043@student.hcmue.edu.vn>
2025-12-10 19:08:36 +0700

    hoan thanh trang 2

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const menubarBtn = document.querySelector(".header-top i");
-menubarBtn.addEventListener("click", function () {
-  document.querySelector(".header-top ul").classList.toggle("active");
+const menuBtn = document.querySelector(".header-top i");
+const menu = document.querySelector(".header-top ul");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
+
+// Đóng menu khi bấm ra ngoài
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+    menu.classList.remove("active");
+    document.body.classList.remove("no-scroll");
+  }
 });
