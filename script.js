185cabb27bb615e090d828bea42a18931d319bb1
KhangTG <5101104043@student.hcmue.edu.vn>
2025-12-12 14:46:17 +0700

    up date giao dien lan 1

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ document.addEventListener("click", (e) => {
 });
 
 // ================= POPUP TÌM KIẾM =================
-document.querySelector("form button").addEventListener("click", function (event) {
-  event.preventDefault(); 
-  document.getElementById("popup").style.display = "flex";
-});
 
 document.getElementById("closePopup").addEventListener("click", function () {
   document.getElementById("popup").style.display = "none";
@@ -80,4 +76,61 @@ document.getElementById("contactForm").addEventListener("submit", async function
 });
 document.getElementById("closePopupContact").addEventListener("click", function () {
     document.getElementById("popupContact").style.display = "none";
+});
+//--fix ngay di ngay ve--//
+const today = new Date().toISOString().split("T")[0];
+
+  // Ngày đi
+  document.querySelector("input[name='departure']")?.setAttribute("min", today);
+
+  // Ngày về
+  document.querySelector("input[name='return']")?.setAttribute("min", today);
+  
+const departureInput = document.querySelector("input[name='departure']");
+const returnInput = document.querySelector("input[name='return']");
+
+// Khi chọn ngày đi → tự cập nhật ngày về cho hợp lệ
+departureInput?.addEventListener("change", function () {
+  returnInput.value = "";
+  returnInput.setAttribute("min", this.value);
+});
+
+// xử lí nút chọn địa điểm cho phù hợp //
+
+const formSearch = document.getElementById("searchForm");
+const locationSelect = document.getElementById("locationSelect");
+
+formSearch.addEventListener("submit", function (e) {
+  e.preventDefault(); // chặn gửi form mặc định
+
+  const location = locationSelect.value;
+
+  if (!location) {
+    alert("Vui lòng chọn địa điểm!");
+    return;
+  }
+
+  // Nếu chọn "Khác"  hiện popup cảm ơn như cũ
+  if (location === "other") {
+    document.getElementById("popup").style.display = "flex";
+    return;
+  }
+
+  // địa điểm → trang con tương ứng
+  const pages = {
+    hanoi: "Tour-hanoi.html",
+    hcm: "Tour-hcm.html",
+    taybac: "Tour-taybac.html",
+    phuquoc: "Tour-phuquoc.html",
+    nhatrang: "Tour-nhatrang.html",
+    dalat: "Tour-dalat.html",
+    quangninh: "Tour-quangninh.html",
+    hue: "Tour-hue.html",
+    hagiang: "Tour-hagiang.html",
+  };
+
+  // Điều hướng sang trang con
+  if (pages[location]) {
+    window.location.href = pages[location];
+  }
 });
